240f58a2730a9c55ce34e50ed0eca452f5a75f76
Ognjen Radisic <radisicognjen@gmail.com>
2020-11-01 23:55:15 +0100

    first update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+//CSS
 import './index.css'
 
+//DIfferent book objects
+const bookOne = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg',
+  author: 'George Orwell',
+  title: '1984 (Signet Classics)',
+}
+
+const bookTwo = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/81zBVMvSjNL._AC_UL200_SR200,200_.jpg',
+  author: 'Stephenie Meyer',
+  title: 'Midnight Sun',
+}
+
+//BookList component that contains multiple 'Book' components
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={bookOne.img} title={bookOne.title} author={bookOne.author} />
+      <Book img={bookTwo.img} title={bookTwo.title} author={bookTwo.author} />
     </section>
   )
 }
-function Book() {
+
+//Book component
+function Book(props) {
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg'
-        alt='you have no luck'
-      />
-      <h1 className='title'>1984 (Signet Classics)</h1>
-      <h2>George Orwell </h2>
+      <img src={props.img} alt='you have no luck' />
+      <h1 className='title'>{props.title}</h1>
+      <h2>{props.author}</h2>
     </article>
   )
 }

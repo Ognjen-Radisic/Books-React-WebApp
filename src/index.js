ced5e34d60f4ff93ef65202aa4560abf9e7f714c
Ognjen Radisic <radisicognjen@gmail.com>
2020-11-02 23:13:54 +0100

    added book list

--- src/index.js
+++ src/index.js
@@ -5,44 +5,53 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 //DIfferent book objects
-const bookOne = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg',
-  author: 'George Orwell',
-  title: '1984 (Signet Classics)',
-}
+const books = [
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg',
+    author: 'George Orwell',
+    title: '1984 (Signet Classics)',
+  },
 
-const bookTwo = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/81zBVMvSjNL._AC_UL200_SR200,200_.jpg',
-  author: 'Stephenie Meyer',
-  title: 'Midnight Sun',
-}
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81zBVMvSjNL._AC_UL200_SR200,200_.jpg',
+    author: 'Stephenie Meyer',
+    title: 'Midnight Sun',
+  },
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81Me-r4UL1L._AC_UL200_SR200,200_.jpg',
+    author: 'Wizards RPG Team',
+    title: "Tasha's Cauldron of Everything (D&D Rules Expansion)",
+  },
 
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg',
+    author: 'Greenlights',
+    title: 'Matthew McConaughey',
+  },
+]
 //BookList component that contains multiple 'Book' components
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book img={bookOne.img} title={bookOne.title} author={bookOne.author}>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut, sed
-          ipsam.
-        </p>
-      </Book>
-      <Book img={bookTwo.img} title={bookTwo.title} author={bookTwo.author} />
+      {books.map((elem) => {
+        return <Book book={elem} />
+      })}
     </section>
   )
 }
 
 //Book component
 function Book(props) {
-  const { img, author, title } = props
+  const { img, author, title } = props.book
   return (
     <article className='book'>
       <img src={img} alt='you have no luck' />
       <h1 className='title'>{title}</h1>
       <h2>{author}</h2>
-      {props.children}
     </article>
   )
 }
